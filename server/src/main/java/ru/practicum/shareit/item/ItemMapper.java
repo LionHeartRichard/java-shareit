da598e55608f2b2c7d9834206bae0bbfa032dbf4
LionHeartRichard <leonhearthrich@gmail.com>
2025-12-23 15:28:49 +0300

    One test Postman Failed by Items With Request!

--- server/src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -35,7 +35,7 @@ public class ItemMapper {
 				.request(request)
 				.available(dto.getAvailable())
 				.description(dto.getDescription())
-				.owner(null).build();
+				.build();
 		return ans;
 	}
 
